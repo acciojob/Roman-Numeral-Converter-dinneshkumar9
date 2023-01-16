f9fd49ccfc0c7f5cdab144fbe78bdb6156174351
Dinesh kumar <dinneshkumar9@gmail.com>
2023-01-16 23:18:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,14 +10,21 @@ function convertToRoman(num) {
     };
 
   //your code here
-let romanNumeral = "";
+let result = '';
+
 for (let i = 0; i < Object.keys(obj).length; i++) {
-while (num >= obj[i][1]) {
-romanNumeral += obj[i][0];
-num -= obj[i][1];
+let currentNum = num;
+while (currentNum >= obj[i][1]) {
+result += obj[i][0];
+currentNum -= obj[i][1];
 }
+num = currentNum;
 }
-return romanNumeral;
+
+// handle special cases for 4 and 9
+result = result.replace("IIII", "IV").replace("VIIII", "IX").replace("XXXX", "XL").replace("LXXXX", "XC").replace("CCCC", "CD").replace("DCCCC", "CM");
+
+return result;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
